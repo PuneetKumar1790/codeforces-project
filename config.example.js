6789cf1e1071d59f65df78b993fcf7a852b0f483
Puneet Kumar <puneetk49081@gmail.com>
2025-04-14 16:19:29 +0530

    merge_solved

--- config.example.js
+++ config.example.js
@@ -1,37 +1,16 @@
-// config.example.js - Example Firebase Configuration (DO NOT USE IN PRODUCTION)
 const firebaseConfig = {
-<<<<<<< Updated upstream
-   apiKey: "AIzaSyB5PmC_MVog8h3apY_dihiWtE-W7B_wz5o",
+  apiKey: "AIzaSyB5PmC_MVog8h3apY_dihiWtE-W7B_wz5o",
   authDomain: "codeforces-1cd9c.firebaseapp.com",
   projectId: "codeforces-1cd9c",
   storageBucket: "codeforces-1cd9c.firebasestorage.app",
   messagingSenderId: "725740122293",
   appId: "1:725740122293:web:e5fdbdb25becf8793a4efc",
   measurementId: "G-PKN5WRHG50"
-
 };
 
 // Prevent duplicate initialization
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 } else {
-    firebase.app();
+  firebase.app();
 }
-=======
-    apiKey: "AIzaSyB5PmC_MVog8h3apY_dihiWtE-W7B_wz5o",
-   authDomain: "codeforces-1cd9c.firebaseapp.com",
-   projectId: "codeforces-1cd9c",
-   storageBucket: "codeforces-1cd9c.firebasestorage.app",
-   messagingSenderId: "725740122293",
-   appId: "1:725740122293:web:e5fdbdb25becf8793a4efc",
-   measurementId: "G-PKN5WRHG50"
- 
- };
- 
- // Prevent duplicate initialization
- if (!firebase.apps.length) {
-     firebase.initializeApp(firebaseConfig);
- } else {
-     firebase.app();
- }
->>>>>>> Stashed changes
